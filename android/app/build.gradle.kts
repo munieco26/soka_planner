dc710c7d5241e2ef30d67bdca98abe913fe4b113
Marcelo Gallardo <mgallardo@mmlab.ar>
2026-03-23 18:54:58 -0300

    [main] migrate from sgiar to soka-planner

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,17 +38,23 @@ android {
     }
 
     signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String?
-            keyPassword = keystoreProperties["keyPassword"] as String?
-            storeFile = file(keystoreProperties["storeFile"] as String?)
-            storePassword = keystoreProperties["storePassword"] as String?
+        if (keystorePropertiesFile.exists()) {
+            create("release") {
+                keyAlias = keystoreProperties["keyAlias"] as String?
+                keyPassword = keystoreProperties["keyPassword"] as String?
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String?
+            }
         }
     }
 
     buildTypes {
         getByName("release") {
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig = if (keystorePropertiesFile.exists()) {
+                signingConfigs.getByName("release")
+            } else {
+                signingConfigs.getByName("debug")
+            }
             isMinifyEnabled = false
             isShrinkResources = false
         }
--- android/app/src/main/kotlin/com/mmlab/sokaplanner/MainActivity.kt
+++ android/app/src/main/kotlin/com/mmlab/sokaplanner/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sgiar.agenda
+package com.mmlab.sokaplanner
 
 import io.flutter.embedding.android.FlutterActivity
 
